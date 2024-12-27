6dfcaeb8978a890de2a89f96021f490d3e350490
Michael Yun Zheng <michaelyzhengcs@gmail.com>
2024-12-27 04:11:48 +0000

    fixes v8

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -21,10 +21,12 @@ export const connectToDatabase = async () => {
     if (!MONGODB_URL) throw new Error('MONGODB_URL is not defined');
 
     cached.promise = cached.promise || mongoose.connect(MONGODB_URL, { 
-     bufferCommands: false 
+        dbName: 'lanmai', bufferCommands: false 
     }); 
 
     cached.conn = await cached.promise;
+    console.log('Successfully connected to the database');
+
 
     return cached.conn;
 }
