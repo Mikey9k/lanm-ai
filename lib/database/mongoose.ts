a29255b9e7a0eda59fb3c66143b2d3610cfb0e76
Michael Yun Zheng <michaelyzhengcs@gmail.com>
2024-12-27 03:47:30 +0000

    fixes v6

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,6 +1,6 @@
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.ATLAS_URI;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
     conn: Mongoose | null;
