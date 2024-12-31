f9ceb9e5359ef1dbbaca88eb6ff81a49d3428b03
Michael Yun Zheng <michaelyzhengcs@gmail.com>
2024-12-31 06:23:41 +0000

    form stuff

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,15 @@
+"use client"
+
 import React from 'react'
+import NavBar from '@/components/shared/NavBar'
 
 
 const Layout = ({children} : {children: React.ReactNode}) => {
   return (
     <main className="root">
+      <NavBar />
+
+
       <div className="root-container">
         <div className="wrapper">
           {children}
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,38 @@
+// import React from 'react';
+// import Header from '@/components/shared/Header';
+// import { transformationTypes } from '@/constants';
+
+// const AddTransformationTypePage = async ({ params: { type } }: SearchParamProps) => {
+
+//   // Safeguard for invalid `type`
+//   const transformation = transformationTypes[type];
+
+//   if (!transformation) {
+//     return (
+//       <Header
+//         title="Transformation Not Found"
+//         subtitle="The specified transformation type does not exist."
+//       />
+//     );
+//   }
+
+//   return (
+//     <Header
+//       title={transformation.title}
+//       subtitle={transformation.subTitle}
+//     />
+//   );
+// };
+
+// export default AddTransformationTypePage;
+
+
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default AddTransformationTypePage
